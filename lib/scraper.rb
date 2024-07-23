3d0e6e8b5eb31dc06fa6d3acb7e1f47df7271643
Cayla Horsey <cayla.horsey@powerhrg.com>
2024-07-23 13:18:18 -0400

    Complete lab

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,4 +1,15 @@
 require 'nokogiri'
 require 'open-uri'
 
-html = open("https://flatironschool.com/")
+url = "https://flatironschool.com/"
+html = URI.open(url)
+
+doc = Nokogiri::HTML(html)
+
+puts doc.css(".heading-primary").text.strip
+
+courses = doc.css(".dropdown-item")
+
+courses.each do |course|
+  puts course.text.strip
+end
